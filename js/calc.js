40f3a3735ff64a7744fdac02d1d7d0c55d9bb5c2
naomih0 <123221320+naomih0@users.noreply.github.com>
2024-02-24 00:55:01 -0600

    Added operate function for the 4 basic arithmetic operations

--- js/calc.js
+++ js/calc.js
@@ -20,3 +20,30 @@ function divide(num1, num2) {
     return precision(total);
 }
 
+function operate(num1, operator, num2) {
+    let results;
+    switch (operator) {
+        case '+':
+            results = add(num1, num2);
+            return results;
+        case '-':
+            results = subtract(num1, num2);
+            return results;
+        case '*':
+            results = multiply(num1, num2);
+            return results;
+        case '/':
+            results = divide(num1, num2);
+            return results;
+        default:
+            return 'Invalid operator'
+    }
+}
+
+
+let num1 = 2;
+let operator = 'o';
+let num2 = 1.2;
+
+console.log(operate(num1, operator, num2))
+
